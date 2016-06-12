3dbaff37421ba61ae8073104517bf10bb89e0607
Kiko Marrs <findkiko@gmail.com>
2016-06-12 14:33:52 -0700

    initial commit

---
+++ slack2particle.rb
@@ -0,0 +1,17 @@
+#!/usr/bin/env ruby
+
+require 'dotenv'
+require 'sinatra'
+require 'httparty'
+
+# Load secrets from local .env file
+Dotenv.load(".env.production")
+
+post "/#{ENV['LISTENER_WEBHOOK_ENDPOINT']}" do
+  request.body.rewind
+  if params[:token] != ENV['SLACK_SECRET_TOKEN'] || params[:channel_id] != SEND_CHANNEL_ID
+    exit
+  end
+  HTTParty.post(ENV['PARTICLE_API_ENDPOINT'], body: {access_token: "#{ENV['PARTICLE_SECRET_TOKEN']}", params: 'on'})
+  #puts "#{params[:first]}"
+end
