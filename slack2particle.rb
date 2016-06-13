2a88c467565624fc1beb7567786289253eca2471
Kiko Marrs <findkiko@gmail.com>
2016-06-12 17:38:00 -0700

    Classify app and switch to local testing mode

--- slack2particle.rb
+++ slack2particle.rb
@@ -4,14 +4,16 @@ require 'dotenv'
 require 'sinatra'
 require 'httparty'
 
-# Load secrets from local .env file
-Dotenv.load(".env.production")
+class Slack2Particle < Sinatra::Base
+  # Load secrets from local .env file
+  Dotenv.load(".env.production")
 
-post "/#{ENV['LISTENER_WEBHOOK_ENDPOINT']}" do
-  request.body.rewind
-  if params[:token] != ENV['SLACK_SECRET_TOKEN'] || params[:channel_id] != SEND_CHANNEL_ID
-    exit
+  post "/#{ENV['LISTENER_WEBHOOK_ENDPOINT']}" do
+    request.body.rewind
+    if params[:token] != ENV['SLACK_SECRET_TOKEN'] or params[:channel_id] != ENV['SLACK_SEND_CHANNEL_ID']
+      exit
+    end
+    #HTTParty.post(ENV['PARTICLE_API_ENDPOINT'], body: {access_token: "#{ENV['PARTICLE_SECRET_TOKEN']}", params: 'on'})
+    puts "#{params[:channel_id]}"
   end
-  HTTParty.post(ENV['PARTICLE_API_ENDPOINT'], body: {access_token: "#{ENV['PARTICLE_SECRET_TOKEN']}", params: 'on'})
-  #puts "#{params[:first]}"
 end
