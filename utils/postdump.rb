5ffceafe7d74207a7348c78ea7d47bc036263b9f
Kiko Marrs <findkiko@gmail.com>
2016-06-16 17:25:21 -0700

    Make PostDump a class

--- utils/postdump.rb
+++ utils/postdump.rb
@@ -2,9 +2,11 @@
 
 require 'sinatra'
 
-post "/" do
-  request.body.rewind
-  params.each do |key, value|
-    puts "#{key}: #{value}"
+class PostDump < Sinatra::Base
+  post "/" do
+    request.body.rewind
+    params.each do |key, value|
+      puts "#{key}: #{value}"
+    end
   end
 end
