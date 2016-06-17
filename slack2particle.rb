192072f9fd04dda1db6adbcc122becb29bcb9d95
Kiko Marrs <findkiko@gmail.com>
2016-06-16 18:19:54 -0700

    return json and auth user_id instead of channel_id

--- slack2particle.rb
+++ slack2particle.rb
@@ -10,10 +10,11 @@ class Slack2Particle < Sinatra::Base
 
   post "/#{ENV['LISTENER_WEBHOOK_ENDPOINT']}" do
     request.body.rewind
-    if params[:token] != ENV['SLACK_SECRET_TOKEN'] or params[:channel_id] != ENV['SLACK_SEND_CHANNEL_ID']
+    if params[:token] != ENV['SLACK_SECRET_TOKEN'] or params[:user_id] != ENV['SLACK_USER_ID']
+      '{ "text": "Sorry: User not authorized." }'
       exit
     end
     #HTTParty.post(ENV['PARTICLE_API_ENDPOINT'], body: {access_token: "#{ENV['PARTICLE_SECRET_TOKEN']}", params: 'on'})
-    puts "#{params[:channel_id]}"
+    '{ "text": "The door has been unlocked." }'
   end
 end
